5744cc54f966e5edee9c5e5d782c7249b4bbf8f4
vordyk <vordy260@gmail.com>
2025-05-29 23:02:53 +0300

    first prepare

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ import mongoose from "mongoose";
 import { registerValidation, loginValidation } from "./validation.js";
 import * as userController from "./controllers/UserController.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Получаем __dirname в ES-модулях
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 mongoose
     .connect('mongodb+srv://vordy262:roma2128@cluster0.8zcdijr.mongodb.net/tableofcalories?retryWrites=true&w=majority&appName=Cluster0')
@@ -12,23 +18,29 @@ mongoose
 const app = express();
 
 app.use(cors({
-    origin: "*", // Можно указать конкретный домен, например: "http://example.com"
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-const port= 4000; // порт 3000 занят реактом
-
 app.use(express.json());
 
+// API-роуты
 app.post('/reg', registerValidation, userController.register);
 app.post('/login', loginValidation, userController.login);
 app.get('/checkAuth', userController.checkAuth);
-
 app.delete('/users', userController.deleteUser);
-
 app.get('/settings', userController.getSettings);
 
+// Раздача статики React-приложения
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Для поддержки client-side routing (SPA)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+const port = process.env.PORT || 4000;
 app.listen(port, (e) => {
-    console.log((!e) ? ('listening at localhost:'+port) : ("fuck up? " + e.message));
-})
+    console.log((!e) ? ('listening at localhost:' + port) : ("fuck up? " + e.message));
+});
